6668a28367c5da42a05866485aa01e1e163b18a4
ShreyasVH <shreyas.deve.1@gmail.com>
2026-02-28 08:28:54 +0530

    updated start and stop scripts

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,13 @@
-kubectl apply -f pod.yaml
-kubectl wait --for=condition=Ready pod/spring-boot-kubernates --timeout=120s
-kubectl port-forward pod/spring-boot-kubernates ${PORT}:${CONTAINER_PORT} > portForward.log 2>&1 &
+PID_FILE=spring-boot-portforward.pid
+if [[ ! -f "$PID_FILE" ]]; then
+    echo "Starting"
+    
+	kubectl apply -f pod.yaml
+	kubectl wait --for=condition=Ready pod/spring-boot-kubernates --timeout=120s
+	kubectl port-forward pod/spring-boot-kubernates ${PORT}:${CONTAINER_PORT} > portForward.log 2>&1 &
+
+	PF_PID=$!
+	echo $PF_PID > "$PID_FILE"
+
+	echo $PF_PID
+fi
--- stop.sh
+++ stop.sh
@@ -1,2 +1,9 @@
-kubectl delete pod spring-boot-kubernates
-kill -9 $(lsof -t -i:${PORT})
+PID_FILE=spring-boot-portforward.pid
+if [[ -f "$PID_FILE" ]]; then
+    echo "Stopping"
+
+	kill -9 $(cat ${PID_FILE})
+	rm ${PID_FILE}
+
+	kubectl delete pod spring-boot-kubernates
+fi
